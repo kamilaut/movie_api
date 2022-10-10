9133d136625e29311bdc7948cd82f5e78879b483
kamilaut <kamila.utyukova@gmail.com>
2022-10-10 11:36:29 +0200

    require express

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 const express = require('express');
 const app = express();
+
+// GET requests
+app.get('/', (req, res) => {
+  res.send('Welcome to my book club!');
+});
+
+app.get('/documentation', (req, res) => {
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.get('/books', (req, res) => {
+  res.json(topBooks);
+});
+
+
+// listen for requests
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
